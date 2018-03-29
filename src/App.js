e3faaa3014473533a93ccbe1ec1de8258746878c
Alejandro Alfonso <alejandro@tiendanube.com>
2018-03-29 18:21:42 -0300

    Add the rest of the itinerary

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import img_kyoto1 from './img/kyoto1.jpg';
 import img_kyoto2 from './img/kyoto2.jpg';
 import img_nara from './img/nara.jpg';
 import img_yoshinoyama from './img/yoshinoyama.jpg';
+import img_hiroshima from './img/hiroshima.jpg';
+import img_miyajima from './img/miyajima.jpg';
+import img_fukuoka from './img/fukuoka.jpg';
+import img_shinkansen from './img/shinkansen.jpg';
+import img_fuji from './img/fuji.jpg';
+import img_tokyo4 from './img/tokyo4.jpg';
 
 class App extends Component {
   render() {
@@ -56,8 +62,8 @@ class App extends Component {
       {
         "date": "Apr 07",
         "title": "Kyoto #1",
-        "highlights": ["Kiyomizudera", "Higashiyama", "Gion", "Nanzenji", "Philosopher's Path", "Kinkakuji"],
-        "lunch": null,
+        "highlights": ["Higashiyama", "Gion", "Philosopher's Path", "Kinkakuji"],
+        "lunch": "Hachidaime Gihey",
         "dinner": "Shabutei",
         "image": img_kyoto1
       },
@@ -73,8 +79,8 @@ class App extends Component {
         "date": "Apr 09",
         "title": "Nara",
         "highlights": ["Tsukiji Market", "Yanesen", "Nakano", "Sangenjaya"],
-        "lunch": "Street Food",
-        "dinner": "Sangenjaya",
+        "lunch": null,
+        "dinner": "Okonomiyaki",
         "image": img_nara
       },
       {
@@ -84,6 +90,54 @@ class App extends Component {
         "lunch": null,
         "dinner": null,
         "image": img_yoshinoyama
+      },
+      {
+        "date": "Apr 11",
+        "title": "Hiroshima",
+        "highlights": ["Shukkeien", "Hiroshima Castle", "Peace Memorial Park", "Hijiyama Park"],
+        "lunch": "Coco Ichibanya",
+        "dinner": "Okonomimura",
+        "image": img_hiroshima
+      },
+      {
+        "date": "Apr 12",
+        "title": "Miyajima",
+        "highlights": ["Itsukushima Shrine", "Daishoin", "Mt Misen"],
+        "lunch": null,
+        "dinner": null,
+        "image": img_miyajima
+      },
+      {
+        "date": "Apr 13",
+        "title": "Fukuoka",
+        "highlights": ["Kawachi Fujien / Iwakuni", "Shofukuji", "Uminonakamichi Park", "Fukuoka Castle"],
+        "lunch": null,
+        "dinner": "Yatai Stalls",
+        "image": img_fukuoka
+      },
+      {
+        "date": "Apr 14",
+        "title": "Trip to Tokyo",
+        "highlights": ["Okayama", "Himeji", "Kobe"],
+        "lunch": null,
+        "dinner": null,
+        "image": img_shinkansen
+      },
+      {
+        "date": "Apr 15",
+        "title": "Mt Fuji",
+        "highlights": ["Shibazakura Festival", "Chureito Pagoda"],
+        "lunch": null,
+        "dinner": "Uobei Sushi",
+        "image": img_fuji
+      },
+      {
+        "date": "Apr 16",
+        "title": "Tokyo #4",
+        "highlights": ["Imperial Palace", "Ginza", "Flight to NY"],
+        "lunch": "Tokyo Station",
+        "dinner": null,
+        "image": img_tokyo4
       },
     ]
 
