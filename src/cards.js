a27bbb82208ef7741eb6ff68f44da3a333a36286
Alejandro Alfonso <alejandro@tiendanube.com>
2018-03-30 10:01:55 -0300

    Extract cards to external file

---
+++ src/cards.js
@@ -0,0 +1,140 @@
+import img_shinjuku from './img/shinjuku.jpg';
+import img_shibuya from './img/shibuya.jpg';
+import img_akiba from './img/akiba.jpg';
+import img_yanesen from './img/yanesen.jpg';
+import img_kanazawa from './img/kanazawa.jpg';
+import img_kyoto1 from './img/kyoto1.jpg';
+import img_kyoto2 from './img/kyoto2.jpg';
+import img_nara from './img/nara.jpg';
+import img_yoshinoyama from './img/yoshinoyama.jpg';
+import img_hiroshima from './img/hiroshima.jpg';
+import img_miyajima from './img/miyajima.jpg';
+import img_fukuoka from './img/fukuoka.jpg';
+import img_shinkansen from './img/shinkansen.jpg';
+import img_fuji from './img/fuji.jpg';
+import img_tokyo4 from './img/tokyo4.jpg';
+
+const cards = [
+  {
+    "date": "Apr 02",
+    "title": "Tokyo #0",
+    "highlights": ["Shinjuku"],
+    "lunch": null,
+    "dinner": "Golden Gai",
+    "image": img_shinjuku
+  },
+  {
+    "date": "Apr 03",
+    "title": "Tokyo #1",
+    "highlights": ["Harajuku","Yoyogi Park","Shibuya"],
+    "lunch": "Ichiran",
+    "dinner": "Uobei Sushi",
+    "image": img_shibuya
+  },
+  {
+    "date": "Apr 04",
+    "title": "Tokyo #2",
+    "highlights": ["Asakusa", "Akihabara", "Ikebukuro"],
+    "lunch": "Hoppy Street",
+    "dinner": "Namja Town",
+    "image": img_akiba
+  },
+  {
+    "date": "Apr 05",
+    "title": "Tokyo #3",
+    "highlights": ["Tsukiji Market", "Yanesen", "Nakano", "Sangenjaya"],
+    "lunch": "Street Food",
+    "dinner": "Sangenjaya",
+    "image": img_yanesen
+  },
+  {
+    "date": "Apr 06",
+    "title": "Kanazawa",
+    "highlights": ["Kenrokuen", "Omicho Market", "Higashi Chaya District", "Trip to Osaka"],
+    "lunch": null,
+    "dinner": "Dotonbori",
+    "image": img_kanazawa
+  },
+  {
+    "date": "Apr 07",
+    "title": "Kyoto #1",
+    "highlights": ["Kinkakuji", "Philosopher's Path", "Gion", "Higashiyama", "Kiyomizudera"],
+    "lunch": null,
+    "dinner": "Shabutei",
+    "image": img_kyoto1
+  },
+  {
+    "date": "Apr 08",
+    "title": "Kyoto #2",
+    "highlights": ["Fushimi Inari", "Nijo Castle", "Arashiyama"],
+    "lunch": "Fire Ramen",
+    "dinner": "Alice in Fantasyland",
+    "image": img_kyoto2
+  },
+  {
+    "date": "Apr 09",
+    "title": "Nara",
+    "highlights": ["Nara Park", "Naramachi", "Pendings in Osaka"],
+    "lunch": null,
+    "dinner": "Okonomiyaki",
+    "image": img_nara
+  },
+  {
+    "date": "Apr 10",
+    "title": "Yoshinoyama",
+    "highlights": ["Yoshinoyama", "Mt. Koya"],
+    "lunch": null,
+    "dinner": null,
+    "image": img_yoshinoyama
+  },
+  {
+    "date": "Apr 11",
+    "title": "Hiroshima",
+    "highlights": ["Shukkeien", "Hiroshima Castle", "Peace Memorial Park", "Hijiyama Park"],
+    "lunch": "Coco Ichibanya",
+    "dinner": "Okonomimura",
+    "image": img_hiroshima
+  },
+  {
+    "date": "Apr 12",
+    "title": "Miyajima",
+    "highlights": ["Itsukushima Shrine", "Daishoin", "Mt Misen"],
+    "lunch": null,
+    "dinner": null,
+    "image": img_miyajima
+  },
+  {
+    "date": "Apr 13",
+    "title": "Fukuoka",
+    "highlights": ["Kawachi Fujien / Iwakuni", "Shofukuji", "Uminonakamichi Park", "Fukuoka Castle"],
+    "lunch": null,
+    "dinner": "Yatai Stalls",
+    "image": img_fukuoka
+  },
+  {
+    "date": "Apr 14",
+    "title": "Trip to Tokyo",
+    "highlights": ["Okayama", "Himeji", "Kobe"],
+    "lunch": null,
+    "dinner": null,
+    "image": img_shinkansen
+  },
+  {
+    "date": "Apr 15",
+    "title": "Mt Fuji",
+    "highlights": ["Shibazakura Festival", "Chureito Pagoda"],
+    "lunch": null,
+    "dinner": "Uobei Sushi",
+    "image": img_fuji
+  },
+  {
+    "date": "Apr 16",
+    "title": "Tokyo #4",
+    "highlights": ["Imperial Palace", "Ginza", "Flight to NY"],
+    "lunch": "Tokyo Station",
+    "dinner": null,
+    "image": img_tokyo4
+  },
+]
+
+export default cards;
